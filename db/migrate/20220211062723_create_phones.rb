46f863a15219ed7624c574c4a91d19dc3f5abdae
Jessica Pinheiro <jessica.pinheiro@codeminer42.com>
2022-02-11 03:39:55 -0300

    define and run phone migrations

--- db/migrate/20220211062723_create_phones.rb
+++ db/migrate/20220211062723_create_phones.rb
@@ -1,10 +1,12 @@
 class CreatePhones < ActiveRecord::Migration[6.1]
   def change
     create_table :phones do |t|
-      t.string :number
+      t.string :number, null: false
       t.string :number_type
-      t.boolean :main_number
+      t.boolean :main_number, null: false
+      t.references :contact, foreign_key: true
 
+      t.index [:number, :contact], unique: true
       t.timestamps
     end
   end
