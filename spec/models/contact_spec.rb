ee20563e1d3cc5ffb04b2654b0806a4fefa02271
Jessica Pinheiro <jessica.pinheiro@codeminer42.com>
2022-02-11 03:23:34 -0300

    write and run tests to contacts

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
-RSpec.describe Contact, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Contact, :type => :model do
+
+  subject {
+    User.create( name: 'Sarah', age: 29, bio: 'Love cats and photography.' )
+  }
+
+    it 'is valid with valid attributes' do
+      subject
+      contact = subject.contacts.create( name: 'Jess', birthday: 1992-11-9 )
+      expect(contact).to be_valid
+    end
+
+    it 'is not valid with a duplicated name' do
+      subject
+      contact2 = subject.contacts.create( name: 'John', birthday: 1994-1-5 )
+      contact3 = subject.contacts.create( name: 'John', birthday: 1993-4-30 )
+      expect(contact3.errors.count).to eq(1)
+    end
+
 end
